b5e0eaa4a7f82c7826263e5b39d4bdbd86efdabf
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2018-02-23 07:50:54 +0100

    Fixed the namespaces that Liblog uses

--- Src/LiquidProjections.PollingEventStore/LibLog.cs
+++ Src/LiquidProjections.PollingEventStore/LibLog.cs
@@ -40,23 +40,23 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "LiquidProjections.NEventStore.Logging")]
-[assembly: SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Scope = "member", Target = "LiquidProjections.NEventStore.Logging.Logger.#Invoke(LiquidProjections.NEventStore.Logging.LogLevel,System.Func`1<System.String>,System.Exception,System.Object[])")]
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "LiquidProjections.PollingEventStoreAdapter.Logging")]
+[assembly: SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Scope = "member", Target = "LiquidProjections.PollingEventStoreAdapter.Logging.Logger.#Invoke(LiquidProjections.PollingEventStoreAdapter.Logging.LogLevel,System.Func`1<System.String>,System.Exception,System.Object[])")]
 
 // If you copied this file manually, you need to change all "YourRootNameSpace" so not to clash with other libraries
 // that use LibLog
 #if LIBLOG_PROVIDERS_ONLY
-namespace LiquidProjections.NEventStore.LibLog
+namespace LiquidProjections.PollingEventStoreAdapter.LibLog
 #else
-namespace LiquidProjections.NEventStore.Logging
+namespace LiquidProjections.PollingEventStoreAdapter.Logging
 #endif
 {
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
 #if LIBLOG_PROVIDERS_ONLY
-    using LiquidProjections.NEventStore.LibLog.LogProviders;
+    using LiquidProjections.PollingEventStoreAdapter.LibLog.LogProviders;
 #else
-    using LiquidProjections.NEventStore.Logging.LogProviders;
+    using LiquidProjections.PollingEventStoreAdapter.Logging.LogProviders;
 #endif
     using System;
 #if !LIBLOG_PROVIDERS_ONLY
@@ -714,9 +714,9 @@ namespace LiquidProjections.NEventStore.Logging
 }
 
 #if LIBLOG_PROVIDERS_ONLY
-namespace LiquidProjections.NEventStore.LibLog.LogProviders
+namespace LiquidProjections.PollingEventStoreAdapter.LibLog.LogProviders
 #else
-namespace LiquidProjections.NEventStore.Logging.LogProviders
+namespace LiquidProjections.PollingEventStoreAdapter.Logging.LogProviders
 #endif
 {
     using System;
